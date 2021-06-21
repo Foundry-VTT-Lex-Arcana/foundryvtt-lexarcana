fdd8ce9c46ea219afad6f841efbba183f6629441
pplans <pierre.plans@gmail.com>
2021-06-21 23:10:20 +0200

    provinces drag and drop from item menu working

--- module/actor/sheets/custos.js
+++ module/actor/sheets/custos.js
@@ -1,6 +1,6 @@
 import LexArcanaActorSheet from "./base.js";
 import LexArcanaActor from "../entity.js";
-import {LexArcana} from '../../config.js';
+import { LexArcana } from '../../config.js';
 
 /**
  * An Actor sheet for player character type actors.
@@ -9,46 +9,91 @@ import {LexArcana} from '../../config.js';
  */
 export default class LexArcanaCustosActorSheet extends LexArcanaActorSheet
 {
-  /**
-   * Define default rendering options for the NPC sheet
-   * @return {Object}
-   */
-  static get defaultOptions()
-  {
-    return mergeObject(super.defaultOptions,
-    {
-      classes: ['LexArcana', 'sheet', 'actor', LexArcana.ActorType.custos],
-      width: 720,
-      height: 680
-    });
-  }
+	/**
+	 * Define default rendering options for the NPC sheet
+	 * @return {Object}
+	 */
+	static get defaultOptions()
+	{
+		return mergeObject(super.defaultOptions,
+			{
+				classes: ['LexArcana', 'sheet', 'actor', LexArcana.ActorType.custos],
+				width: 720,
+				height: 680
+			});
+	}
 
-  /* -------------------------------------------- */
+	/** @override */
+	getData()
+	{
+		// Basic data
+		const data = super.getData();
+		// Iterate through items, allocating to containers
+		for (let i of data.actor.items)
+		{
+			let item = i.data;
+			switch (i.type)
+			{
+				case 'province':
+					data.province = i;
+					break;
+				default: break;
+			}
+		}
+		return data;
+	}
 
-  /* -------------------------------------------- */
-  /*  Event Listeners and Handlers
-  /* -------------------------------------------- */
+	/* -------------------------------------------- */
 
-  /**
-   * Activate event listeners using the prepared sheet HTML
-   * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
-   */
-  activateListeners(html)
-  {
-    super.activateListeners(html);
-    if ( !this.options.editable ) return;
-    // Rollable sheet actions
-    html.find('.rollable').click(this._onSheetAction.bind(this));
-  }
+	/* -------------------------------------------- */
+	/*  Event Listeners and Handlers
+	/* -------------------------------------------- */
 
-  /**
-   * Handle mouse click events for character sheet actions
-   * @param {MouseEvent} event    The originating click event
-   * @private
-   */
-  _onSheetAction(event)
-  {
-    event.preventDefault();
-    //const dataset = event.currentTarget.dataset;
-  }
+	/**
+	 * Activate event listeners using the prepared sheet HTML
+	 * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
+	 */
+	activateListeners(html)
+	{
+		super.activateListeners(html);
+		if (!this.options.editable) return;
+
+		// Drag events for macros.
+		if (this.actor.isOwner)
+		{
+			let handler = (ev) => this._onDragStart(ev);
+			// Find all items on the character sheet.
+			html.find('li.item').each((i, li) =>
+			{
+				// Ignore for the header row.
+				if (li.classList.contains('item-header')) return;
+				// Add draggable attribute and dragstart listener.
+				li.setAttribute('draggable', true);
+				li.addEventListener('dragstart', handler, false);
+			});
+		}
+	}
+
+	/* -------------------------------------------- */
+	
+	/* -------------------------------------------- */
+	/*  Overrides
+	/* -------------------------------------------- */
+	async _onDropItem(event, data)
+	{
+		// Iterate through items, allocating to containers
+		let itemIds = [];
+		for (let i of this.actor.items)
+		{
+			switch (i.type)
+			{
+				case 'province':
+					itemIds.push(i.id);
+					break;
+				default: break;
+			}
+		}
+		await this.actor.deleteEmbeddedDocuments('Item', itemIds);
+		return super._onDropItem(event, data);
+	}
 }
